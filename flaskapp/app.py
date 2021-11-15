88d80619905e50260f567805dcb278c1e09ea56c
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-11-15 09:31:08 -0500

    Create Python app

---
+++ flaskapp/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+db = Redis(host='db', port=6379)
+
+@app.route('/')
+def index():
+    try:
+        hitCount = db.incr('hitCount')
+        return f'Hello there! This page has been accessed {hitCount} times.'
+    except:
+        return 'Hello there! We couldn\'t reach the redis instance so we don\'t know how many times this page has been accessed.'
